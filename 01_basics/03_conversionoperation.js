4ff735ed63c281888b41ed19c292d224991370b0
illusion-16 <kritisans2005@gmail.com>
2026-02-21 21:27:28 +0530

    conversion of datatypes

---
+++ 01_basics/03_conversionoperation.js
@@ -0,0 +1,31 @@
+let score = 33
+
+
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueInString = String(score) //converting number to string
+console.log(typeof valueInString);
+
+let score1 = "33abc";
+let valueInNumber = Number(score1) 
+console.log(typeof valueInNumber); // shows thats output is number
+console.log(valueInNumber) //when we check the value it shows NaN which means not a no.
+// while converting it can show that type is a no. but when we check the value it might not be the number 
+
+//"33" =>33
+// "33abc" => NaN
+// true =>1; false => 0 
+
+let isLoggedIn = 1;
+let booleanLoggedIn = Boolean(isLoggedIn)
+console.log(booleanLoggedIn)
+
+// 1 => true ; 0 => false 
+// "" => false 
+// "sanskriti" => true
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(stringNumber)
+console.log(typeof stringNumber); // even though  the outut is a number thee type of outpuut is string 
